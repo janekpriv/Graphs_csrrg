d1b68fdf222bea3a6018cd1bb0759fe7afadade7
Janek <janekpriv@gmail.com>
2025-03-27 10:50:10 +0100

    updated lib files

--- lib/parse_csr.h
+++ lib/parse_csr.h
@@ -1,8 +1,10 @@
 #ifndef PARSE_CSR_H
 #define PARSE_CSR_H
 
+#include "graph.h"
+
 void parse(char* filename);
-Graph *generate_graph_from_csrr(char* filname);
+Graph *generate_graph_from_csrrg(char* filname);
 int get_number_of_nodes(char *a_r);
 int *tokenize(char *edges_offset, int c_count);
 void convert(char *edges, int *e_offset, Graph* g);
