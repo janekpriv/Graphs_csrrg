8fdb2ce1af0b62b0603a8ebfeb51fdac49de2af2
Janek <janekpriv@gmail.com>
2025-04-03 00:30:58 +0200

    header file for louvian

---
+++ lib/louvian.h
@@ -0,0 +1,11 @@
+#ifndef LOUVIAN_H
+#define LOUVIAN_H
+
+#include "graph.h"
+
+// communities is a list of all nodes where node is the index and value represents certain community 
+double get_modularity(int *communities, Graph *g);
+
+
+
+#endif
