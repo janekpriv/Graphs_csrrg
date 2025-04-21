28ffd3e463dcf5381da95766736d623df5f6efaf
nstprod <milana.prod@gmail.com>
2025-04-21 12:09:12 +0200

    add missing headers

--- lib/louvian.h
+++ lib/louvian.h
@@ -11,10 +11,12 @@ typedef struct List{
 // communities is a list of all nodes where node is the index and value represents certain community 
 double get_modularity(int *communities, Graph *g);
 
-void louvain(Graph *g, int desired_k);
-void phase1(Graph *temporary_graph, Graph *original_graph, int target_comm_count);
+int louvain(Graph *g);
+void phase1(Graph *temporary_graph, Graph *original_graph);
 Graph* phase2(Graph *g);
 double delta_modularity(int community, Node u, Graph *g);
 void seed_communities(Graph *g);
 void print_communities(Graph * g);
+void merge_to_three_communities(Graph *g, int comm_count);
+void change_communities(Graph *g, int old_comm, int new_comm);
 #endif
